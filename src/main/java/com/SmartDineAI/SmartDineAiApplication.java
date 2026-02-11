da79032b87ddcc2ac5457232be064d20f81958e6
TrongPhuc209 <nguyentrongphuc1125@gmail.com>
2026-02-11 14:33:10 +0700

    first commit

---
+++ src/main/java/com/SmartDineAI/SmartDineAiApplication.java
@@ -0,0 +1,13 @@
+package com.SmartDineAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDineAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDineAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartDineAI/SmartDineAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartDineAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDineAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
